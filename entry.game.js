f9be2f68edd617688977deaa29acacb55bb94207
tyller <103223674+TyllerTheGamer@users.noreply.github.com>
2025-06-14 00:33:47 -0500

    Added raw api and proper game entry, raw api needs testing

--- entry.game.js
+++ entry.game.js
@@ -1,6 +1,18 @@
 fluxloaderAPI.events.on("fl:scene-loaded", (scene) => {
     if (scene == 3) { // in game, store should be loaded
-        let buildings = gameInstance.state.store.player.buildings;
+        // we remove undefined so incase a mod tech gave an undefined building it doesn't crash the game
+        let buildings = fluxloaderAPI.gameWorld.state.store.player.buildings;
+        buildings = buildings.filter(building => building !== undefined);
     }
 });
 
+// please rework these names
+globalThis.rawAPI ??= {
+    variable: {},
+    function: {},
+    named: {
+        variable: {},
+        function: {}
+    }
+}
+fluxloaderAPI.events.registerEvent("cl:raw-api-setup");

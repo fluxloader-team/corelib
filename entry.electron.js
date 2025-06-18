a36741564c61e5aa7a43633f6de489c25cd47216
tomster12 <thomas.burniston@gmail.com>
2025-06-18 21:29:19 +0100

    First commit to start seperating out

--- entry.electron.js
+++ entry.electron.js
@@ -33,34 +33,14 @@ class CoreLib {
 			type: "replace",
 			from: `e.fps;function Af`,
 			to: `
-globalThis.corelib.exposed.function = {kf,Cf,Ef,Tf,_f,Sf,wf,bf,xf,vf,gf,
-df,hf,cf,uf,lf,af,of,sf,rf,nf,tf,
-ef,Jd,Qd,Zd,Kd,qd,Yd,$d,Xd,Wd,Hd,
-Vd,Gd,Ud,jd,zd,Od,Bd,Ld,Nd,Fd,Dd,
-Id,Rd,Pd,Md,Ad,kd,Cd,Td,_d,Sd,wd,
-bd,xd,vd,yd,gd,ld,ad,od,sd,id,rd,
-td,ed,Jh,Qh,Zh,Kh,qh,Yh,$h,zh,Qh,
-Bh,Lh,Nh,Fh,Dh,Ih,Rh,Ah,kh,Eh,Th,
-_h,bh,xh,vh,yh,gh,ch,lh,ah,ih,nh,
-th,eh,Jc,Qc,qc,Yc,$c,Xc,Wc,Hc,Vc,
-Gc,Uc,jc,zc,Oc,Bc,Lc,Nc,Fc,Dc,Ic,
-Rc,Pc,Mc,Ac,kc,Ec,wc,bc,xc,yc,gc,
-mc,pc,fc,dc,hc,cc,uc,lc,tc,ec,Ju,
-Qu,Zu,Ku,$u,Hu,Vu,Uu,ju,zu,Ou,Lu,
-Nu,Fu,Du,Iu,Ru,Pu,ku,Eu,Tu,_u,Su,
-bu,xu,vu,yu,gu,pu,fu,du,lu,au,ou,
-ru,nu,tu,eu,Ql,Zl,Kl,ql,Yl,$l,Ul,
-jl,zl,Ol,Bl,Ll,Nl,Fl,Dl,Il,Rl,Pl,
-Ml,Al};
-globalThis.corelib.exposed.variable = {Ed,nd,Xh,Wh,Hh,Gh,jh,wh,vc,oc,Yu,Xu,Wu,Gu,Mu,Au,wu,mu,hu};
-globalThis.corelib.exposed.named.function = {clearCell: (x, y) => {ud(fluxloaderAPI.gameWorld.state, y ? {x, y} : {x: x.x, y: x.y})},
-setCell: (x, y, element) => {Od(fluxloaderAPI.gameWorld.state, x, y, element)},
-makePixel: (x, y, element) => {return Fh(element, x, y, undefined)},
-multiplyVector: (vector, mutliplier) => {return yc(vector, mutliplier)},
-checkCellsEqual: (element1, element2) => {return Kd(element1, element2)},
-checkClearOfDynamic: (x, y) => {tf(fluxloaderAPI.gameWorld.state, x, y)},
-GetStaticCell: (x, y) => {return sf(fluxloaderAPI.gameWorld.state, x, y)},
-}
+globalThis.corelib.exposed = {
+kf,Cf,Ef,Tf,_f,Sf,wf,bf,xf,vf,gf,df,hf,cf,uf,lf,af,of,sf,rf,nf,tf,ef,Jd,Qd,Zd,Kd,qd,Yd,$d,Xd,Wd,Hd,
+Vd,Gd,Ud,jd,zd,Od,Bd,Ld,Nd,Fd,Dd,Id,Rd,Pd,Md,Ad,kd,Cd,Td,_d,Sd,wd,bd,xd,vd,yd,gd,ld,ad,od,sd,id,rd,
+td,ed,Jh,Qh,Zh,Kh,qh,Yh,$h,zh,Qh,Bh,Lh,Nh,Fh,Dh,Ih,Rh,Ah,kh,Eh,Th,_h,bh,xh,vh,yh,gh,ch,lh,ah,ih,nh,
+th,eh,Jc,Qc,qc,Yc,$c,Xc,Wc,Hc,Vc,Gc,Uc,jc,zc,Oc,Bc,Lc,Nc,Fc,Dc,Ic,Rc,Pc,Mc,Ac,kc,Ec,wc,bc,xc,yc,gc,
+mc,pc,fc,dc,hc,cc,uc,lc,tc,ec,Ju,Qu,Zu,Ku,$u,Hu,Vu,Uu,ju,zu,Ou,Lu,Nu,Fu,Du,Iu,Ru,Pu,ku,Eu,Tu,_u,Su,
+bu,xu,vu,yu,gu,pu,fu,du,lu,au,ou,ru,nu,tu,eu,Ql,Zl,Kl,ql,Yl,$l,Ul,jl,zl,Ol,Bl,Ll,Nl,Fl,Dl,Il,Rl,Pl,
+Ml,Al,Ed,nd,Xh,Wh,Hh,Gh,jh,wh,vc,oc,Yu,Xu,Wu,Gu,Mu,Au,wu,mu,hu};
 fluxloaderAPI.events.tryTrigger("cl:raw-api-setup");
 ~`,
 			token: `~`,
--- entry.game.js
+++ entry.game.js
@@ -1,11 +1,28 @@
-globalThis.corelib = {
-	exposed: {
-		variable: {},
-		function: {},
-		named: {
-			variable: {},
-			function: {},
-		},
+globalThis.corelib = {};
+
+globalThis.corelib.exposed = {};
+
+globalThis.corelib.simulation = {
+	clearCell: (x, y) => {
+		globalThis.corelib.exposed.ud(fluxloaderAPI.gameWorld.state, y ? { x, y } : { x: x.x, y: x.y });
+	},
+	setCell: (x, y, element) => {
+		globalThis.corelib.exposed.Od(fluxloaderAPI.gameWorld.state, x, y, element);
+	},
+	makePixel: (x, y, element) => {
+		return globalThis.corelib.exposed.Fh(element, x, y, undefined);
+	},
+	multiplyVector: (vector, mutliplier) => {
+		return globalThis.corelib.exposed.yc(vector, mutliplier);
+	},
+	checkCellsEqual: (element1, element2) => {
+		return globalThis.corelib.exposed.Kd(element1, element2);
+	},
+	checkClearOfDynamic: (x, y) => {
+		globalThis.corelib.exposed.tf(fluxloaderAPI.gameWorld.state, x, y);
+	},
+	getStaticCell: (x, y) => {
+		return globalThis.corelib.exposed.sf(fluxloaderAPI.gameWorld.state, x, y);
 	},
 };
 

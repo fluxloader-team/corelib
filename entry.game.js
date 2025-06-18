10b619826a8448536610660f701ee19fc7d466f0
tomster12 <thomas.burniston@gmail.com>
2025-06-19 00:04:31 +0100

    Got basic block and particles working

--- entry.game.js
+++ entry.game.js
@@ -1,29 +1,42 @@
-globalThis.corelib = {};
-
-globalThis.corelib.exposed = {};
-
-globalThis.corelib.simulation = {
-	clearCell: (x, y) => {
-		globalThis.corelib.exposed.ud(fluxloaderAPI.gameWorld.state, y ? { x, y } : { x: x.x, y: x.y });
-	},
-	setCell: (x, y, element) => {
-		globalThis.corelib.exposed.Od(fluxloaderAPI.gameWorld.state, x, y, element);
-	},
-	makePixel: (x, y, element) => {
-		return globalThis.corelib.exposed.Fh(element, x, y, undefined);
-	},
-	multiplyVector: (vector, mutliplier) => {
-		return globalThis.corelib.exposed.yc(vector, mutliplier);
-	},
-	checkCellsEqual: (element1, element2) => {
-		return globalThis.corelib.exposed.Kd(element1, element2);
-	},
-	checkClearOfDynamic: (x, y) => {
-		globalThis.corelib.exposed.tf(fluxloaderAPI.gameWorld.state, x, y);
-	},
-	getStaticCell: (x, y) => {
-		return globalThis.corelib.exposed.sf(fluxloaderAPI.gameWorld.state, x, y);
-	},
-};
+globalThis.corelib = { exposed: {} };
 
 fluxloaderAPI.events.registerEvent("cl:raw-api-setup");
+
+fluxloaderAPI.events.on("cl:raw-api-setup", () => {
+	log("info", "corelib", "Setting up corelib raw API");
+	corelib.simulation.internal = {};
+	corelib.simulation.internal.solids = corelib.exposed.t;
+	corelib.simulation.internal.particles = corelib.exposed.n;
+	corelib.simulation.internal.blocks = corelib.exposed.d;
+	corelib.simulation.internal.createParticle = corelib.exposed.Fh;
+	corelib.simulation.internal.createBlock = corelib.exposed.xd;
+	corelib.simulation.internal.setElement = corelib.exposed.Od;
+});
+
+corelib.simulation = {
+	// ---------------- Elements, cells, particles, solids ----------------
+	spawnParticle: (x, y, type, data = {}) => {
+		const particleType = corelib.simulation.internal.particles[type];
+		if (particleType === undefined) log("error", "corelib", `Particle type ${type} does not exist!`);
+		const particle = corelib.simulation.internal.createParticle(particleType, x, y, data);
+		corelib.simulation.internal.setElement(fluxloaderAPI.gameInstance.state, x, y, particle);
+	},
+	spawnMovingParticle: (x, y, vx, vy, type, data = {}) => {
+		const particleType = corelib.simulation.internal.particles[type];
+		if (particleType === undefined) log("error", "corelib", `Particle type ${type} does not exist!`);
+		const innerParticle = corelib.simulation.internal.createParticle(particleType, x, y, data);
+		const outerParticle = corelib.simulation.internal.createParticle(corelib.simulation.internal.particles.Particle, x, y, {
+			element: innerParticle,
+			velocity: { x: vx, y: vy },
+		});
+		corelib.simulation.internal.setElement(fluxloaderAPI.gameInstance.state, x, y, outerParticle);
+	},
+	spawnBlock: (x, y, type) => {
+		const blockType = corelib.simulation.internal.blocks[type];
+		if (blockType === undefined) log("error", "corelib", `Block type ${type} does not exist!`);
+		corelib.simulation.internal.createBlock(fluxloaderAPI.gameInstance.state, { x, y }, { structureType: blockType });
+	},
+	revealFog: (x, y) => {
+		fluxloaderAPI.gameInstance.state.environment.multithreading.simulation.postAll(fluxloaderAPI.gameInstance.state, [14, x, y]);
+	},
+};

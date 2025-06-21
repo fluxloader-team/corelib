81c9d0b882acbe3fa9ab8df14585d74abeaff88a
tomster12 <thomas.burniston@gmail.com>
2025-06-21 18:48:45 +0100

    nit

--- entry.worker.js
+++ entry.worker.js
@@ -1,6 +1,6 @@
 await import("./shared.game.worker.js");
 
-//events: worker-cell-changed
+// events: worker-cell-changed
 corelib.events.processCellChange = (_worker, _x, _y, _from, _to) => {
 	if (_worker === undefined || _x === undefined || _y === undefined || _from === undefined || _to === undefined) {
 		return;
@@ -13,7 +13,7 @@ corelib.events.processCellChange = (_worker, _x, _y, _from, _to) => {
 	fluxloaderAPI.sendGameMessage("corelib:eventMessage", eventMessage);
 };
 
-//events: worker-fog-reveal
+// events: worker-fog-reveal
 corelib.events.processFogReveal = (_worker, _x, _y) => {
 	if (_worker === undefined || _x === undefined || _y === undefined) {
 		return;

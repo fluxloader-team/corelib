64c69111af67a35f3bd925c21f8f0ff41ec9be8f
tomster12 <thomas.burniston@gmail.com>
2025-10-13 22:27:58 +0100

    Fix variant issues

--- modules/blocks.js
+++ modules/blocks.js
@@ -171,9 +171,10 @@ class BlocksModule {
 		let id = data.parentId + data.suffix;
 		let parentBlock = this.registry.entries[data.parentId];
 		let fullImagePath = this.getFullImagePath(parentBlock.sourceMod, id, data.imagePath);
+		let variantData = { isVariant: true, fullImagePath, id, ...data };
 
-		if (this.registry.register(data.id, { isVariant: true, fullImagePath, ...data })) {
-			parentBlock.variants.push({ fullImagePath, ...data });
+		if (this.registry.register(id, variantData)) {
+			parentBlock.variants.push(variantData);
 		}
 	}
 
@@ -267,16 +268,13 @@ class BlocksModule {
 			to:
 				reduceBlocks(
 					(b) =>
-						`if(n.type===d.${b.id}){l=t.session.rendering.images["${b.fullImagePath}"],(u=e.snapGridCellSize*e.cellSize),(c=Nf(t,n.x*e.cellSize,n.y*e.cellSize));h.drawImage(l.image,l.image.height*(Math.floor(t.store.meta.time/${
-							b.animationDelay || 500
+						`if(n.type===d.${b.id}){l=t.session.rendering.images["${b.fullImagePath}"],(u=e.snapGridCellSize*e.cellSize),(c=Nf(t,n.x*e.cellSize,n.y*e.cellSize));h.drawImage(l.image,l.image.height*(Math.floor(t.store.meta.time/${b.animationDelay || 500
 						})%(l.image.width/l.image.height)),0,l.image.height,l.image.height,c.x,c.y,u,u);}else ` +
 						reduceBlockVariants(
 							b,
 							(v) =>
-								`if(n.type===d.${b.id}){l=t.session.rendering.images["${
-									v.fullImagePath
-								}"],(u=e.snapGridCellSize*e.cellSize),(c=Nf(t,n.x*e.cellSize,n.y*e.cellSize));h.drawImage(l.image,l.image.height*(Math.floor(t.store.meta.time/${
-									v.animationDelay || 500
+								`if(n.type===d.${b.id}){l=t.session.rendering.images["${v.fullImagePath
+								}"],(u=e.snapGridCellSize*e.cellSize),(c=Nf(t,n.x*e.cellSize,n.y*e.cellSize));h.drawImage(l.image,l.image.height*(Math.floor(t.store.meta.time/${v.animationDelay || 500
 								})%(l.image.width/l.image.height)),0,l.image.height,l.image.height,c.x,c.y,u,u);}else `,
 						),
 				) + "~",

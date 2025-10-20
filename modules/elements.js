d7a10767bf1e8dbe45414b222d24b07a8f3056d9
Almighty Pumpkin <dracothedragon135@gmail.com>
2025-10-20 09:58:01 -0700

    in proggress function

--- modules/elements.js
+++ modules/elements.js
@@ -348,13 +348,13 @@ class ElementsModule {
 		/*
 		fluxloaderAPI.setPatch("js/336.bundle.js", "corelib:burningFuctionEdit", {
    			type: "replace",
-   			from: `p=((a={})[i.RJ.Slag]=function(){return{output:{elementType:i.RJ.BurntSlag,chance:.25}}},a),f=!1,g=function(e,t,r,a){if(!(t<0||r<0||t>=e.store.world.size.width||r>=e.store.world.size.height)){var n=(0,c.tT)(e.store,t,r);if((0,c.Ol)(n))return(d=(0,s.n)(i.RJ.Fire,t,r)).duration.left=d.duration.max=d.duration.left*Math.max(.25,1-a/.64),void(0,c.Jx)(e,t,r,d);if((0,c.W)(n,i.vZ.Ice))(0,u.jE)(e,t,r);else{if((0,c.af)(n,[i.RJ.Water,i.RJ.FreezingIce])){var d=(0,s.n)(i.RJ.Steam,t,r);return(0,c.Jx)(e,t,r,d),f||(e.environment.postMessage([i.dD.ForceCompleteObjective,"vaporize_water"]),f=!0),void e.environment.postMessage([i.dD.PlaySound,[{id:"vaporize",opts:{volume:.1,fadeOut:l.A.getRandomFloatBetween(.1,.5),playbackRate:l.A.getRandomFloatBetween(.5,1.5)},modulateDistance:{x:d.x*o.A.cellSize,y:d.y*o.A.cellSize}}]])}if((0,c.af)(n,i.RJ.Slag))return(d=(0,s.n)(i.RJ.Flame,t,r)).data=p[i.RJ.Slag](),d.duration.left=d.duration.max=d.duration.left-a,void(0,c.Jx)(e,t,r,d);x(e,t,r,n)||(0,c.af)(n,i.RJ.Basalt)&&(0,c.Jx)(e,t,r,(0,s.n)(i.RJ.Lava,t,r))}}},y=function(e,t){var r;if(![i.RJ.Flame,i.RJ.Lava].includes(t.type))return!1;if(t.type===i.RJ.Flame&&((0,v.$T)(e,t.x*o.A.cellSize,t.y*o.A.cellSize,v.c6.Fire),e.environment.postMessage([i.dD.AddLight,t.x*o.A.cellSize,t.y*o.A.cellSize,{brightness:1,duration:100,useLightZones:!0}])),[{x:1,y:0},{x:-1,y:0},{x:0,y:1},{x:0,y:-1}].forEach((function(r){var a=r.x,n=r.y,o={cX:t.x+a,cY:t.y+n},s=o.cX,d=o.cY;if(!(s<0||d<0||s>=e.store.world.size.width||d>=e.store.world.size.height)){var u=e.environment,c=l.A.getThreadIndexFromCellX(s,u.threadMeta.threadCount);c===u.threadMeta.startingIndex?m(e,s,d,t):e.environment.threadMeta.ports[c].postMessage([i.dD.Burn,s,d])}})),t.type===i.RJ.Lava)return t.duration.left=t.duration.max*l.A.getRandomFloatBetween(.5,1.5),t.variantIndex=l.A.getRandomIntBetween(0,3),(0,c.Jx)(e,t.x,t.y,t),!0;var a=null===(r=t.data)||void 0===r?void 0:r.output;return a?!1===a.elementType||a.chance&&Math.random()>=a.chance?((0,c.Jx)(e,t.x,t.y,(0,s.n)(i.RJ.Fire,t.x,t.y)),!0):((0,c.Jx)(e,t.x,t.y,(0,s.n)(a.elementType,t.x,t.y)),!0):((0,c.Jx)(e,t.x,t.y,(0,s.n)(i.RJ.Fire,t.x,t.y)),!0)},m=function(e,t,r,a){var n=(0,c.tT)(e.store,t,r);if((0,c.Ol)(n)){var l=(null==a?void 0:a.type)===i.RJ.Lava?.01:.25;if(Math.random()<l){(null==a?void 0:a.type)===i.RJ.Lava&&e.environment.postMessage([i.dD.AddLight,a.x*o.A.cellSize,a.y*o.A.cellSize,{brightness:1,size:r*o.A.cellSize<e.store.world.horizon[Math.floor(t)]*o.A.cellSize+10*o.A.cellSize?100:1e3,duration:5e3,useLightZones:!0}]);var d=(0,s.n)(i.RJ.Fire,t,r);return(null==a?void 0:a.type)===i.RJ.Lava&&(d.data.temperature=1200),void(0,c.Jx)(e,t,r,d)}}if((0,c.af)(n,i.RJ.Slag)){var u=(0,s.n)(i.RJ.Flame,t,r);return u.data=p[i.RJ.Slag](),void(0,c.Jx)(e,t,r,u)}(0,h.x)(e,n,t,r),x(e,t,r,n)},S=((n={})[i.vZ.Moss]=!1,n[i.vZ.Divider]=!1,n[i.vZ.GoldSoil]=i.RJ.Gold,n[i.vZ.Petal]=i.RJ.Petalium,n),x=function(e,t,r,a){if(a=null!=a?a:(0,c.tT)(e.store,t,r),(0,c.ez)(a)){if((0,c.kw)(a,[i.vZ.Moss,i.vZ.Divider,i.vZ.GoldSoil,i.vZ.Petal])){var n=(0,s.n)(i.RJ.Flame,t,r);return n.skipPhysics=!0,n.data={output:{elementType:S[a]}},(0,c.Jx)(e,t,r,n),!0}(0,d.zT)(e,t,r,4)}return!1}}`,
+   			from: `p=((a={})[i.RJ.Slag]=function(){return{output:{elementType:i.RJ.BurntSlag,chance:.25}}},a),f=!1,g=function(e,t,r,a){if(!(t<0||r<0||t>=e.store.world.size.width||r>=e.store.world.size.height)){var n=(0,c.tT)(e.store,t,r);if((0,c.Ol)(n))return(d=(0,s.n)(i.RJ.Fire,t,r)).duration.left=d.duration.max=d.duration.left*Math.max(.25,1-a/.64),void(0,c.Jx)(e,t,r,d);if((0,c.W)(n,i.vZ.Ice))(0,u.jE)(e,t,r);else{if((0,c.af)(n,[i.RJ.Water,i.RJ.FreezingIce])){var d=(0,s.n)(i.RJ.Steam,t,r);return(0,c.Jx)(e,t,r,d),f||(e.environment.postMessage([i.dD.ForceCompleteObjective,"vaporize_water"]),f=!0),void e.environment.postMessage([i.dD.PlaySound,[{id:"vaporize",opts:{volume:.1,fadeOut:l.A.getRandomFloatBetween(.1,.5),playbackRate:l.A.getRandomFloatBetween(.5,1.5)},modulateDistance:{x:d.x*o.A.cellSize,y:d.y*o.A.cellSize}}]])}if((0,c.af)(n,i.RJ.Slag))return(d=(0,s.n)(i.RJ.Flame,t,r)).data=p[i.RJ.Slag](),d.duration.left=d.duration.max=d.duration.left-a,void(0,c.Jx)(e,t,r,d);x(e,t,r,n)||(0,c.af)(n,i.RJ.Basalt)&&(0,c.Jx)(e,t,r,(0,s.n)(i.RJ.Lava,t,r))}}},y=function(e,t){var r;if(![i.RJ.Flame,i.RJ.Lava].includes(t.type))return!1;if(t.type===i.RJ.Flame&&((0,v.$T)(e,t.x*o.A.cellSize,t.y*o.A.cellSize,v.c6.Fire),e.environment.postMessage([i.dD.AddLight,t.x*o.A.cellSize,t.y*o.A.cellSize,{brightness:1,duration:100,useLightZones:!0}])),[{x:1,y:0},{x:-1,y:0},{x:0,y:1},{x:0,y:-1}].forEach((function(r){var a=r.x,n=r.y,o={cX:t.x+a,cY:t.y+n},s=o.cX,d=o.cY;if(!(s<0||d<0||s>=e.store.world.size.width||d>=e.store.world.size.height)){var u=e.environment,c=l.A.getThreadIndexFromCellX(s,u.threadMeta.threadCount);c===u.threadMeta.startingIndex?m(e,s,d,t):e.environment.threadMeta.ports[c].postMessage([i.dD.Burn,s,d])}})),t.type===i.RJ.Lava)return t.duration.left=t.duration.max*l.A.getRandomFloatBetween(.5,1.5),t.variantIndex=l.A.getRandomIntBetween(0,3),(0,c.Jx)(e,t.x,t.y,t),!0;var a=null===(r=t.data)||void 0===r?void 0:r.output;return a?!1===a.elementType||a.chance&&Math.random()>=a.chance?((0,c.Jx)(e,t.x,t.y,(0,s.n)(i.RJ.Fire,t.x,t.y)),!0):((0,c.Jx)(e,t.x,t.y,(0,s.n)(a.elementType,t.x,t.y)),!0):((0,c.Jx)(e,t.x,t.y,(0,s.n)(i.RJ.Fire,t.x,t.y)),!0)},m=function(e,t,r,a){var n=(0,c.tT)(e.store,t,r);if((0,c.Ol)(n)){var l=(null==a?void 0:a.type)===i.RJ.Lava?.01:.25;if(Math.random()<l){(null==a?void 0:a.type)===i.RJ.Lava&&e.environment.postMessage([i.dD.AddLight,a.x*o.A.cellSize,a.y*o.A.cellSize,{brightness:1,size:r*o.A.cellSize<e.store.world.horizon[Math.floor(t)]*o.A.cellSize+10*o.A.cellSize?100:1e3,duration:5e3,useLightZones:!0}]);var d=(0,s.n)(i.RJ.Fire,t,r);return(null==a?void 0:a.type)===i.RJ.Lava&&(d.data.temperature=1200),void(0,c.Jx)(e,t,r,d)}}if((0,c.af)(n,i.RJ.Slag)){var u=(0,s.n)(i.RJ.Flame,t,r);return u.data=p[i.RJ.Slag](),void(0,c.Jx)(e,t,r,u)}(0,h.x)(e,n,t,r),x(e,t,r,n)},S=((n={})[i.vZ.Moss]=!1,n[i.vZ.Divider]=!1,n[i.vZ.GoldSoil]=i.RJ.Gold,n[i.vZ.Petal]=i.RJ.Petalium,n),x=function(e,t,r,a){if(a=null!=a?a:(0,c.tT)(e.store,t,r),(0,c.ez)(a)){if((0,c.kw)(a,[i.vZ.Moss,i.vZ.Divider,i.vZ.GoldSoil,i.vZ.Petal])){var n=(0,s.n)(i.RJ.Flame,t,r);return n.skipPhysics=!0,n.data={output:{elementType:S[a]}},(0,c.Jx)(e,t,r,n),!0}(0,d.zT)(e,t,r,4)}return!1}`,
     		to: `
     		p=((a={})[i.RJ.Slag]=function(){return{output:{elementType:i.RJ.BurntSlag,chance:.25}}},a),f=!1,
     		g=function(e,t,r,a){if(!(t<0||r<0||t>=e.store.world.size.width||r>=e.store.world.size.height)){var n=(0,c.tT)(e.store,t,r);if((0,c.Ol)(n))return(d=(0,s.n)(i.RJ.Fire,t,r)).duration.left=d.duration.max=d.duration.left*Math.max(.25,1-a/.64),void(0,c.Jx)(e,t,r,d);if((0,c.W)(n,i.vZ.Ice))(0,u.jE)(e,t,r);else{if((0,c.af)(n,[i.RJ.Water,i.RJ.FreezingIce])){var d=(0,s.n)(i.RJ.Steam,t,r);return(0,c.Jx)(e,t,r,d),f||(e.environment.postMessage([i.dD.ForceCompleteObjective,"vaporize_water"]),f=!0),void e.environment.postMessage([i.dD.PlaySound,[{id:"vaporize",opts:{volume:.1,fadeOut:l.A.getRandomFloatBetween(.1,.5),playbackRate:l.A.getRandomFloatBetween(.5,1.5)},modulateDistance:{x:d.x*o.A.cellSize,y:d.y*o.A.cellSize}}]])}if((0,c.af)(n,i.RJ.Slag))return(d=(0,s.n)(i.RJ.Flame,t,r)).data=p[i.RJ.Slag](),d.duration.left=d.duration.max=d.duration.left-a,void(0,c.Jx)(e,t,r,d);x(e,t,r,n)||(0,c.af)(n,i.RJ.Basalt)&&(0,c.Jx)(e,t,r,(0,s.n)(i.RJ.Lava,t,r))}}},
     		y=function(e,t){var r;if(![i.RJ.Flame,i.RJ.Lava].includes(t.type))return!1;if(t.type===i.RJ.Flame&&((0,v.$T)(e,t.x*o.A.cellSize,t.y*o.A.cellSize,v.c6.Fire),e.environment.postMessage([i.dD.AddLight,t.x*o.A.cellSize,t.y*o.A.cellSize,{brightness:1,duration:100,useLightZones:!0}])),[{x:1,y:0},{x:-1,y:0},{x:0,y:1},{x:0,y:-1}].forEach((function(r){var a=r.x,n=r.y,o={cX:t.x+a,cY:t.y+n},s=o.cX,d=o.cY;if(!(s<0||d<0||s>=e.store.world.size.width||d>=e.store.world.size.height)){var u=e.environment,c=l.A.getThreadIndexFromCellX(s,u.threadMeta.threadCount);c===u.threadMeta.startingIndex?m(e,s,d,t):e.environment.threadMeta.ports[c].postMessage([i.dD.Burn,s,d])}})),t.type===i.RJ.Lava)return t.duration.left=t.duration.max*l.A.getRandomFloatBetween(.5,1.5),t.variantIndex=l.A.getRandomIntBetween(0,3),(0,c.Jx)(e,t.x,t.y,t),!0;var a=null===(r=t.data)||void 0===r?void 0:r.output;return a?!1===a.elementType||a.chance&&Math.random()>=a.chance?((0,c.Jx)(e,t.x,t.y,(0,s.n)(i.RJ.Fire,t.x,t.y)),!0):((0,c.Jx)(e,t.x,t.y,(0,s.n)(a.elementType,t.x,t.y)),!0):((0,c.Jx)(e,t.x,t.y,(0,s.n)(i.RJ.Fire,t.x,t.y)),!0)},
     		m=function(e,t,r,a){var n=(0,c.tT)(e.store,t,r);if((0,c.Ol)(n)){var l=(null==a?void 0:a.type)===i.RJ.Lava?.01:.25;if(Math.random()<l){(null==a?void 0:a.type)===i.RJ.Lava&&e.environment.postMessage([i.dD.AddLight,a.x*o.A.cellSize,a.y*o.A.cellSize,{brightness:1,size:r*o.A.cellSize<e.store.world.horizon[Math.floor(t)]*o.A.cellSize+10*o.A.cellSize?100:1e3,duration:5e3,useLightZones:!0}]);var d=(0,s.n)(i.RJ.Fire,t,r);return(null==a?void 0:a.type)===i.RJ.Lava&&(d.data.temperature=1200),void(0,c.Jx)(e,t,r,d)}}if((0,c.af)(n,i.RJ.Slag)){var u=(0,s.n)(i.RJ.Flame,t,r);return u.data=p[i.RJ.Slag](),void(0,c.Jx)(e,t,r,u)}(0,h.x)(e,n,t,r),x(e,t,r,n)},S=((n={})[i.vZ.Moss]=!1,n[i.vZ.Divider]=!1,n[i.vZ.GoldSoil]=i.RJ.Gold,n[i.vZ.Petal]=i.RJ.Petalium,n),
-    		x=function(e,t,r,a){if(a=null!=a?a:(0,c.tT)(e.store,t,r),(0,c.ez)(a)){if((0,c.kw)(a,[i.vZ.Moss,i.vZ.Divider,i.vZ.GoldSoil,i.vZ.Petal])){var n=(0,s.n)(i.RJ.Flame,t,r);return n.skipPhysics=!0,n.data={output:{elementType:S[a]}},(0,c.Jx)(e,t,r,n),!0}(0,d.zT)(e,t,r,4)}return!1}}`
+    		x=function(e,t,r,a){if(a=null!=a?a:(0,c.tT)(e.store,t,r),(0,c.ez)(a)){if((0,c.kw)(a,[i.vZ.Moss,i.vZ.Divider,i.vZ.GoldSoil,i.vZ.Petal])){var n=(0,s.n)(i.RJ.Flame,t,r);return n.skipPhysics=!0,n.data={output:{elementType:S[a]}},(0,c.Jx)(e,t,r,n),!0}(0,d.zT)(e,t,r,4)}return!1}`
 		}); 
 		*/
 		fluxloaderAPI.setPatch("js/515.bundle.js", "corelib:elements:Press", {
@@ -388,3 +388,107 @@ class ElementsModule {
 }
 
 globalThis.ElementsModule = ElementsModule;
+
+burnableRecipes = {
+		soils: {
+			[i.vZ.Moss]: { output: false },
+			[i.vZ.Divider]: { output: false },
+			[i.vZ.GoldSoil]: { output: { elementType: i.RJ.Gold, chance: 1 } },
+			[i.vZ.Petal]: { output: { elementType: i.RJ.Petalium, chance: 1 } },
+		},
+		particles: {
+			[i.RJ.Slag]: {
+				output: { elementType: i.RJ.BurntSlag, chance: 0.25 },
+			},
+		},
+	},
+hasVaporizedWater = !1,
+g = function (e, t, r, a) {
+		if (!(t < 0 || r < 0 || t >= e.store.world.size.width || r >= e.store.world.size.height)) {
+			var n = (0, c.tT)(e.store, t, r);
+			if ((0, c.Ol)(n)) return ((d = (0, s.n)(i.RJ.Fire, t, r)).duration.left = d.duration.max = d.duration.left * Math.max(0.25, 1 - a / 0.64)), void (0, c.Jx)(e, t, r, d);
+			if ((0, c.W)(n, i.vZ.Ice)) (0, u.jE)(e, t, r);
+			else {
+				if ((0, c.af)(n, [i.RJ.Water, i.RJ.FreezingIce])) {
+					var d = (0, s.n)(i.RJ.Steam, t, r);
+					return (
+						(0, c.Jx)(e, t, r, d),
+						hasVaporizedWater || (e.environment.postMessage([i.dD.ForceCompleteObjective, "vaporize_water"]), (hasVaporizedWater = !0)),
+						void e.environment.postMessage([
+							i.dD.PlaySound,
+							[{ id: "vaporize", opts: { volume: 0.1, fadeOut: l.A.getRandomFloatBetween(0.1, 0.5), playbackRate: l.A.getRandomFloatBetween(0.5, 1.5) }, modulateDistance: { x: d.x * o.A.cellSize, y: d.y * o.A.cellSize } }],
+						])
+					);
+				}
+				if ((0, c.af)(n, i.RJ.Slag)) return ((d = (0, s.n)(i.RJ.Flame, t, r)).data = p[i.RJ.Slag]()), (d.duration.left = d.duration.max = d.duration.left - a), void (0, c.Jx)(e, t, r, d);
+				x(e, t, r, n) || ((0, c.af)(n, i.RJ.Basalt) && (0, c.Jx)(e, t, r, (0, s.n)(i.RJ.Lava, t, r)));
+			}
+		}
+	},
+	y = function (e, t) {
+		var r;
+		if (![i.RJ.Flame, i.RJ.Lava].includes(t.type)) return !1;
+		if (
+			(t.type === i.RJ.Flame &&
+				((0, v.$T)(e, t.x * o.A.cellSize, t.y * o.A.cellSize, v.c6.Fire), e.environment.postMessage([i.dD.AddLight, t.x * o.A.cellSize, t.y * o.A.cellSize, { brightness: 1, duration: 100, useLightZones: !0 }])),
+			[
+				{ x: 1, y: 0 },
+				{ x: -1, y: 0 },
+				{ x: 0, y: 1 },
+				{ x: 0, y: -1 },
+			].forEach(function (r) {
+				var a = r.x,
+					n = r.y,
+					o = { cX: t.x + a, cY: t.y + n },
+					s = o.cX,
+					d = o.cY;
+				if (!(s < 0 || d < 0 || s >= e.store.world.size.width || d >= e.store.world.size.height)) {
+					var u = e.environment,
+						c = l.A.getThreadIndexFromCellX(s, u.threadMeta.threadCount);
+					c === u.threadMeta.startingIndex ? m(e, s, d, t) : e.environment.threadMeta.ports[c].postMessage([i.dD.Burn, s, d]);
+				}
+			}),
+			t.type === i.RJ.Lava)
+		)
+			return (t.duration.left = t.duration.max * l.A.getRandomFloatBetween(0.5, 1.5)), (t.variantIndex = l.A.getRandomIntBetween(0, 3)), (0, c.Jx)(e, t.x, t.y, t), !0;
+		var a = null === (r = t.data) || void 0 === r ? void 0 : r.output;
+		return a
+			? !1 === a.elementType || (a.chance && Math.random() >= a.chance)
+				? ((0, c.Jx)(e, t.x, t.y, (0, s.n)(i.RJ.Fire, t.x, t.y)), !0)
+				: ((0, c.Jx)(e, t.x, t.y, (0, s.n)(a.elementType, t.x, t.y)), !0)
+			: ((0, c.Jx)(e, t.x, t.y, (0, s.n)(i.RJ.Fire, t.x, t.y)), !0);
+	},
+	m = function (e, t, r, a) {
+		var n = (0, c.tT)(e.store, t, r);
+		if ((0, c.Ol)(n)) {
+			var l = (null == a ? void 0 : a.type) === i.RJ.Lava ? 0.01 : 0.25;
+			if (Math.random() < l) {
+				(null == a ? void 0 : a.type) === i.RJ.Lava &&
+					e.environment.postMessage([
+						i.dD.AddLight,
+						a.x * o.A.cellSize,
+						a.y * o.A.cellSize,
+						{ brightness: 1, size: r * o.A.cellSize < e.store.world.horizon[Math.floor(t)] * o.A.cellSize + 10 * o.A.cellSize ? 100 : 1e3, duration: 5e3, useLightZones: !0 },
+					]);
+				var d = (0, s.n)(i.RJ.Fire, t, r);
+				return (null == a ? void 0 : a.type) === i.RJ.Lava && (d.data.temperature = 1200), void (0, c.Jx)(e, t, r, d);
+			}
+		}
+		if ((0, c.af)(n, i.RJ.Slag)) {
+			var u = (0, s.n)(i.RJ.Flame, t, r);
+			return (u.data = p[i.RJ.Slag]()), void (0, c.Jx)(e, t, r, u);
+		}
+		(0, h.x)(e, n, t, r), x(e, t, r, n);
+	},
+	
+	x = function (e, t, r, a) {
+		if (((a = null != a ? a : (0, c.tT)(e.store, t, r)), (0, c.ez)(a))) {
+			if ((0, c.kw)(a, [i.vZ.Moss, i.vZ.Divider, i.vZ.GoldSoil, i.vZ.Petal])) {
+				var n = (0, s.n)(i.RJ.Flame, t, r);
+				return (n.skipPhysics = !0), (n.data = { output: { elementType: S[a] } }), (0, c.Jx)(e, t, r, n), !0;
+			}
+			(0, d.zT)(e, t, r, 4);
+		}
+		return !1;
+	}
+

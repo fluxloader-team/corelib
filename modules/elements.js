071f875c807ddd620e3f5389ce51d79ba187edba
tomster12 <thomas.burniston@gmail.com>
2025-10-23 19:04:59 +0100

    Fix up the registration

--- modules/elements.js
+++ modules/elements.js
@@ -1,62 +1,41 @@
-const recipeSchemas = {
-	basic: {
-		input1: {
-			type: "string",
-		},
-		input2: {
-			type: "string",
-		},
-		output1: {
-			type: "string",
-		},
-		output2: {
-			type: "string",
-			default: "Empty",
-		},
-		addBothWays: {
-			type: "boolean",
-			default: true,
-		},
-	},
-	press: {
-		input: {
-			type: "string",
-		},
-		requiredVelocity: {
-			type: "number",
-			default: 200,
-		},
-		outputs: {
-			type: "array",
-			verifier: (v) => {
-				return {
-					//didn't know every had a value, thanks again chatgpt
-					success: v.every((item) => Array.isArray(item) && typeof item[0] === "string" && typeof item[1] === "number"),
-					message: `Parameter 'outputs' must be an array of arrays with the output in the first and the chance in the second`,
-				};
-			},
-		},
-	},
-	burn: {},
-	shake: {},
+const basicRecipeRegisterSchema = {
+	input1: { type: "string" },
+	input2: { type: "string" },
+	output1: { type: "string" },
+	output2: { type: "string", default: "Empty" },
+	bothWays: { type: "boolean", default: true }
+};
+
+const basicRecipeUnregisterSchema = {
+	input1: { type: "string" },
+	input2: { type: "string" },
+	bothWays: { type: "boolean", default: true }
+};
+
+const pressRecipeRegisterSchema = {
+	input: { type: "string" },
+	requiredVelocity: { type: "number", default: 200 },
+	outputs: {
+		type: "array",
+		verifier: (v) => {
+			return {
+				//didn't know every had a value, thanks again chatgpt
+				success: v.every((item) => Array.isArray(item) && typeof item[0] === "string" && typeof item[1] === "number"),
+				message: `Parameter 'outputs' must be an array of arrays with the output in the first and the chance in the second`,
+			};
+		}
+	}
+};
+
+const pressRecipeUnregisterSchema = {
+	input: { type: "string" }
 };
 
 class ElementsModule {
 	elementRegistry = {};
 	soilRegistry = {};
-
-	elementReactions = {
-		normal: {},
-		press: {},
-		shaker: {},
-		burn: {},
-	};
-
-	otherFeatures = {
-		planterAllows: [],
-		shakerAllows: [],
-		conveyorBeltIgnores: [],
-	};
+	recipes = { basic: {}, press: {} };
+	otherFeatures = { planterAllows: [], shakerAllows: [], conveyorBeltIgnores: [] };
 
 	constructor() {
 		this.registerBasicRecipe("Sand", "Water", "WetSand", "WetSand");
@@ -74,47 +53,46 @@ class ElementsModule {
 		this.registerConveyorBeltIgnores("Fire");
 	}
 
-	registerBasicRecipe(input1, input2, output1, output2, addBothWays /* recipeSchema.basic */) {
-		const schemaCheck = { input1, input2, output1, output2, addBothWays };
-		const data = validateInput(schemaCheck, recipeSchemas.basic, true).data;
+	registerBasicRecipe(inputData /* basicRecipeRegisterSchema */) {
+		const data = validateInput(inputData, basicRecipeRegisterSchema, true).data;
 		const add = (from, to) => {
-			this.elementReactions.normal[from] ??= [];
-			this.elementReactions.normal[from].push([to, data.output1, data.output2]);
+			this.recipes.basic[from] ??= [];
+			this.recipes.basic[from].push([to, data.output1, data.output2]);
 		};
-		if (data.addBothWays) add(data.input1, data.input2);
+		if (data.bothWays) add(data.input1, data.input2);
 		add(data.input2, data.input1);
 	}
 
-	registerPressRecipe(input, outputs, requiredVelocity /* recipeSchema.press */) {
-		const schemaCheck = { input, outputs, requiredVelocity };
-		const data = validateInput(schemaCheck, recipeSchemas.press, true).data;
-		this.elementReactions.press[data.input] = [data.requiredVelocity, data.outputs];
+	unregisterBasicRecipe(inputData /* basicRecipeUnregisterSchema */) {
+		const data = validateInput(inputData, basicRecipeUnregisterSchema, true).data;
+		const removeBasicRecipe = (input1, input2) => {
+			if (!this.recipes.basic[input1]) return log("error", "corelib", `Could not unregister basic recipe with elements "${element1}" and "${element2}"!`);
+			this.recipes.basic[input1] = this.recipes.basic[input1].filter(([target]) => target !== input2);
+			if (this.recipes.basic[input1].length === 0) delete this.recipes.basic[input1];
+		};
+		if (data.bothWays) removeBasicRecipe(data.element1, data.element2);
+		removeBasicRecipe(data.element2, data.element1);
+	}
+
+	registerPressRecipe(inputData /* pressRecipeRegisterSchema */) {
+		const data = validateInput(inputData, pressRecipeRegisterSchema, true).data;
+		this.recipes.press[data.input] = [data.requiredVelocity, data.outputs];
+	}
+
+	unregisterPressRecipe(inputData /* pressRecipeUnregisterSchema */) {
+		const data = validateInput(inputData, pressRecipeUnregisterSchema, true).data;
+		if (!this.recipes.press[data.input]) return log("error", "corelib", `Could not unregister press recipe with id "${data.input}", not found!`);
+		delete this.recipes.press[data.input];
 	}
 
 	registerConveyorBeltIgnores(id) {
 		this.otherFeatures.conveyorBeltIgnores.push(id);
 	}
 
-	unregisterBasicRecipe(element1, element2, removeBothWays = true) {
-		const removeBasicRecipe = (input1, input2) => {
-			if (!this.elementReactions.normal[input1]) return log("error", "corelib", `Could not unregister recipe between ${element1} and ${element2}! The reaction doesn't exist.`);
-			this.elementReactions.normal[input1] = this.elementReactions.normal[input1].filter(([target]) => target !== input2);
-			if (this.elementReactions.normal[input1].length === 0) delete this.elementReactions.normal[input1];
-		};
-		if (removeBothWays) removeBasicRecipe(element1, element2);
-		removeBasicRecipe(element2, element1);
-	}
-
-	unregisterPressRecipe(id) {
-		if (!this.elementReactions.press[id]) return log("error", "corelib", `Press recipe with id "${id}" not found! Unable to unregister.`);
-		delete this.elementReactions.press[id];
-	}
-
 	unregisterConveyorBeltIgnores(id) {
 		const index = this.otherFeatures.conveyorBeltIgnores.indexOf(id);
-		if (index > -1) {
-			this.otherFeatures.conveyorBeltIgnores.splice(index, 1);
-		}
+		if (index == -1) return log("error", "corelib", `Could not unregister conveyorBeltIgnore with id "${id}", not found!`);
+		this.otherFeatures.conveyorBeltIgnores.splice(index, 1);
 	}
 
 	applyPatches() {
@@ -139,7 +117,7 @@ class ElementsModule {
 		fluxloaderAPI.setPatch("js/515.bundle.js", "corelib:elements:basicReactionsList", {
 			type: "replace",
 			from: `c=((i={})[n.RJ.Water]=[[n.RJ.Sand,n.RJ.WetSand],[n.RJ.Spore,n.RJ.WetSpore],[n.RJ.Lava,n.RJ.Steam],[n.RJ.Flame,n.RJ.Steam]],i[n.RJ.Sand]=[[n.RJ.Water,n.RJ.WetSand]],i[n.RJ.Spore]=[[n.RJ.Water,n.RJ.WetSpore]],i[n.RJ.Lava]=[[n.RJ.Water,n.RJ.Steam]],i[n.RJ.Flame]=[[n.RJ.Water,n.RJ.Steam]],i[n.RJ.Sandium]=[[n.RJ.Petalium,n.RJ.Gloom]],i[n.RJ.Petalium]=[[n.RJ.Sandium,n.RJ.Gloom]],i)`,
-			to: `c=((i={}),${getBasicRecipesToPatch(this.elementReactions.normal, "i")},i)`,
+			to: `c=((i={}),${getBasicRecipesToPatch(this.recipes.basic, "i")},i)`,
 		});
 		fluxloaderAPI.setPatch("js/515.bundle.js", "corelib:elements:basicReactionsFunctionChange", {
 			type: "replace",
@@ -151,7 +129,7 @@ class ElementsModule {
 			type: "replace",
 			from: `s=function(e,t,r){return!(r!==n.vZ.VelocitySoaker||t.type!==n.RJ.BurntSlag||t.velocity.y<200||!h(e,t.x,t.y,n.RJ.Spore)||((0,l.Nz)(e,t),h(e,t.x,t.y,n.RJ.Gold),e.environment.postMessage([n.dD.PlaySound,[{id:"coin",opts:{volume:.2,fadeOut:a.A.getRandomFloatBetween(.1,2),playbackRate:a.A.getRandomFloatBetween(.5,1.5)},modulateDistance:{x:t.x*i.A.cellSize,y:t.y*i.A.cellSize}}]]),0))}`,
 			to: `pressRecipes=(function(){var press={};${getPressRecipesToPatch(
-				this.elementReactions.press,
+				this.recipes.press,
 				"press",
 			)};return press;})(),s=function(e,t,r){const recipe=pressRecipes[t.type];if(r!==n.vZ.VelocitySoaker||!recipe||t.velocity.y<recipe[0]){return false;}const outputs=recipe[1];let posY = outputs.length; for(const[outputId,chance]of outputs){if(Math.random()<chance){posY--;h(e,t.x,t.y+posY,outputId);}}(0,l.Nz)(e,t);if(outputs.some(([outputId,_])=>outputId===n.RJ.Gold)){e.environment.postMessage([n.dD.PlaySound,[{id:"coin",opts:{volume:.2,fadeOut:a.A.getRandomFloatBetween(.1,2),playbackRate:a.A.getRandomFloatBetween(.5,1.5)},modulateDistance:{x:t.x*i.A.cellSize,y:t.y*i.A.cellSize}}]])}return true;}`,
 		});
